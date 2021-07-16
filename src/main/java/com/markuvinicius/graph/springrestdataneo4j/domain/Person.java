ce24c8263617f826f1ced30e83cd9d54980a9de2
markuvinicius@gmail.com <markuvinicius@gmail.com>
2021-07-16 20:04:57 -0300

    removing target folder

--- src/main/java/com/markuvinicius/graph/springrestdataneo4j/domain/Person.java
+++ src/main/java/com/markuvinicius/graph/springrestdataneo4j/domain/Person.java
@@ -8,7 +8,7 @@ import org.springframework.data.neo4j.core.schema.*;
 import java.util.Set;
 import java.util.UUID;
 
-@Node("Person")
+@Node(labels = {"Person"})
 @AllArgsConstructor
 public class Person {
 
@@ -29,11 +29,11 @@ public class Person {
     private String cnpj;
 
     @Getter
-    @Relationship(type="IS_PARTNER")
+    @Relationship(type="PARTNER")
     private Set<Partner> partnership;
 
     @Getter
-    @Relationship(type="IS_REPRESENTATIVE")
+    @Relationship(type="REPRESENTATIVE")
     private Set<Representative> representatives;
 
     public void setPartner(Partner partner){
